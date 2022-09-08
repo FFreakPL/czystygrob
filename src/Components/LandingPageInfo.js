163315c4df0c9a5f1293bf178b3445f38d3a0603
Paweł Kubiak <ffreakpl@MacBook-Air-Pawe.local>
2022-09-08 13:12:38 +0200

    opis

--- src/Components/LandingPageInfo.js
+++ src/Components/LandingPageInfo.js
@@ -10,9 +10,9 @@ export default function LandingPageInfo() {
                     największą ilość grobów, które niekiedy są niestety w bardzo złym stanie. Grób jest jedynym miejscem,
                     gdzie możemy przebywać poniekąd w obecności naszych ukochanych zmarłych. Czysty grób świadczy
                     natomiast o pamięci o tych, którzy byli bliscy naszemu sercu. W dzisiejszym świecie nie każdy ma
-                    niestety czas lub możliwości aby upamiętniać bliskich troszcząc się o nagrobek. Z tego względu
+                    niestety czas lub możliwości aby upamiętniać bliskich troszcząc się o ich nagrobek. Z tego względu
                     stworzyliśmy naszą rodzinną firmę - aby ułatwić dbanie o groby bliskich tym wszystkim, którzy o nich
-                    pamiętają i dalej darzą miłością lecz z różnych względów nie mogą należycie dbać o miejsce spoczynku
+                    pamiętają i dalej darzą ich miłością lecz z różnych względów nie mogą należycie dbać o miejsce spoczynku
                     bliskich.
                 </p>
             </article>
